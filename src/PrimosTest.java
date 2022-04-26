bdcb5412fbd57356b23cbb3f6f06a14469a9e8a9
konstanZX2 <zxgenerationzx@gmail.com>
2022-04-26 20:10:29 +0200

    a

---
+++ src/PrimosTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimosTest {
+
+    @org.junit.jupiter.api.Test
+    void generarPrimos() {
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testGenerarPrimos() {
+    }
+}
